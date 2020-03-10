6802ed86af557c47b9a9049b45b441a16c8f5460
Allan <allan.wolfe89@gmail.com>
2020-03-10 13:06:30 -0500

    first

---
+++ src/App.vue
@@ -0,0 +1,128 @@
+<template>
+<div id="app">
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      clipped
+    >
+      <v-list dense>
+        <v-list-item
+          v-for="item in items"
+          :key="item.text"
+          @click=""
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>
+              {{ item.text }}
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-subheader class="mt-3 grey--text text--darken-1">SUBSCRIPTIONS</v-subheader>
+        <v-list>
+          <v-list-item
+            v-for="item in items2"
+            :key="item.text"
+            @click=""
+          >
+            <v-list-item-avatar>
+              <img
+                :src="`https://randomuser.me/api/portraits/men/${item.picture}.jpg`"
+                alt=""
+              >
+            </v-list-item-avatar>
+            <v-list-item-title v-text="item.text"></v-list-item-title>
+          </v-list-item>
+        </v-list>
+        <v-list-item
+          class="mt-3"
+          @click=""
+        >
+          <v-list-item-action>
+            <v-icon color="grey darken-1">add_circle_outline</v-icon>
+          </v-list-item-action>
+          <v-list-item-title class="grey--text text--darken-1">Browse Channels</v-list-item-title>
+        </v-list-item>
+        <v-list-item @click="">
+          <v-list-item-action>
+            <v-icon color="grey darken-1">settings</v-icon>
+          </v-list-item-action>
+          <v-list-item-title class="grey--text text--darken-1">Manage Subscriptions</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar
+      app
+      clipped-left
+      color="blue"
+      dense
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-icon class="mx-3">fab fa-youtube</v-icon>
+      <v-toolbar-title class="mr-5 align-center">
+        <span class="title">Youtube</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-layout
+        row
+        align-center
+        style="max-width: 650px"
+      >
+        <v-text-field
+          :append-icon-cb="() => {}"
+          placeholder="Search..."
+          single-line
+          append-icon="search"
+          color="white"
+          hide-details
+        ></v-text-field>
+      </v-layout>
+    </v-app-bar>
+
+    <v-content>
+    <router-view />
+ 
+    </v-content>
+  </v-app>
+</div>
+    
+ 
+</template>
+
+<script>
+export default {
+  name: 'App',
+    data: () => ({
+    drawer: null,
+    items: [
+      { icon: 'trending_up', text: 'Most Popular' },
+      { icon: 'subscriptions', text: 'Subscriptions' },
+      { icon: 'history', text: 'History' },
+      { icon: 'featured_play_list', text: 'Playlists' },
+      { icon: 'watch_later', text: 'Watch Later' },
+    ],
+    items2: [
+      { picture: 28, text: 'Joseph' },
+      { picture: 38, text: 'Apple' },
+      { picture: 48, text: 'Xbox Ahoy' },
+      { picture: 58, text: 'Nokia' },
+      { picture: 78, text: 'MKBHD' },
+    ],
+  })
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/list.vue
@@ -0,0 +1,172 @@
+<template>
+    
+     <v-container>
+         
+      <v-row dense>
+        <v-col cols="12">
+          <v-card
+            color="#385F73"
+            dark
+          >
+            <v-card-title class="headline">Unlimited music now</v-card-title>
+
+            <v-card-subtitle>Listen to your favorite artists and albums whenever and wherever, online and offline.</v-card-subtitle>
+
+    <v-container class="fill-height">
+      <v-row
+        align="center"
+        justify="center"
+      >
+        <v-btn
+          color="pink"
+          dark
+          @click.stop="drawer = !drawer"
+        >
+          Toggle
+        </v-btn>
+      </v-row>
+    </v-container>
+
+    <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      temporary
+    >
+      <v-list-item>
+        <v-list-item-avatar>
+          <v-img src="https://randomuser.me/api/portraits/men/78.jpg"></v-img>
+        </v-list-item-avatar>
+
+        <v-list-item-content>
+          <v-list-item-title>John Leider</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense>
+
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+            <v-card-actions>
+              <v-btn text>Listen Now</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+ <v-card
+    class="mx-auto"
+    max-width="400"
+  >
+    <v-toolbar
+      flat
+      color="deep-purple accent-4"
+      dark
+    >
+      <v-btn icon>
+        <v-icon>mdi-close</v-icon>
+      </v-btn>
+      <v-toolbar-title>Filter results</v-toolbar-title>
+    </v-toolbar>
+
+    <v-card-text>
+      <h2 class="title mb-2">Choose amenities</h2>
+
+      <v-chip-group
+        v-model="amenities"
+        column
+        multiple
+      >
+        <v-chip filter outlined>Elevator</v-chip>
+        <v-chip filter outlined>Washer / Dryer</v-chip>
+        <v-chip filter outlined>Fireplace</v-chip>
+        <v-chip filter outlined>Wheelchair access</v-chip>
+        <v-chip filter outlined>Dogs ok</v-chip>
+        <v-chip filter outlined>Cats ok</v-chip>
+      </v-chip-group>
+    </v-card-text>
+
+    <v-card-text>
+      <h2 class="title mb-2">Choose neighborhoods</h2>
+
+      <v-chip-group
+        v-model="neighborhoods"
+        column
+        multiple
+      >
+        <v-chip filter outlined>Snowy Rock Place</v-chip>
+        <v-chip filter outlined>Honeylane Circle</v-chip>
+        <v-chip filter outlined>Donna Drive</v-chip>
+        <v-chip filter outlined>Elaine Street</v-chip>
+        <v-chip filter outlined>Court Street</v-chip>
+        <v-chip filter outlined>Kennedy Park</v-chip>
+      </v-chip-group>
+    </v-card-text>
+  </v-card>
+        <v-col
+          v-for="(item, i) in items"
+          :key="i"
+          cols="9"
+        >
+          <v-card
+            :color="item.color"
+            dark
+          >
+            <div class="d-flex flex-no-wrap justify-space-between">
+              <div>
+                <v-card-title
+                  class="headline"
+                  v-text="item.title"
+                ></v-card-title>
+
+                <v-card-subtitle v-text="item.artist"></v-card-subtitle>
+              </div>
+
+              <v-avatar
+                class="ma-3"
+                size="125"
+                tile
+              >
+                <v-img :src="item.src"></v-img>
+              </v-avatar>
+            </div>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+</template>
+
+<script>
+export default {
+    name: 'list',
+    data: () => ({
+      items: [
+        {
+          color: '#1F7087',
+          src: 'https://cdn.vuetifyjs.com/images/cards/foster.jpg',
+          title: 'Supermodel',
+          artist: 'Foster the People',
+        },
+        {
+          color: '#952175',
+          src: 'https://cdn.vuetifyjs.com/images/cards/halcyon.png',
+          title: 'Halcyon Days',
+          artist: 'Ellie Goulding',
+        },
+      ],
+    }),
+}
+</script>
